9ac599de4f2a60f0adf306519188dc37f5a07d01
Tal <tal.matityah1@gmail.com>
2025-03-07 18:14:53 +0200

    Added more pages for each topic

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=21)
-    pdf.set_text_color(100,100,150)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.set_line_width(1)
-    pdf.set_draw_color(100,100,150)
-    pdf.line(10, 21, 200, 21)
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=21)
+        pdf.set_text_color(100,100,150)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+        pdf.set_line_width(1)
+        pdf.set_draw_color(100,100,150)
+        pdf.line(10, 21, 200, 21)
+
 
 
 pdf.output("output.pdf")
